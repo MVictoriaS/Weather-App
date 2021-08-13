bc9bfc32d77f8ac34c14bbff3cf0b35ee7cfa3b6
Victoria Santo <santovik@outlook.com>
2021-08-13 20:10:46 +0100

    first commit

---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h2 className="title-container__title">Weather Finder</h2>
+		<h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+	</div>
+);
+
+export default Titles;
